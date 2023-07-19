a1e70f8046a8ec575a0801a0b9a03e15958ce350
Andrii Boretskyi <boryapoc@gmail.com>
2023-07-19 16:15:01 -0400

    [ADD] Set default value phone fields for new res.partner view form

---
+++ ata_voip/static/src/js/ata_phone_call_details.js
@@ -0,0 +1,79 @@
+/** @odoo-module **/
+
+import config from "web.config";
+import { useBus, useService } from "@web/core/utils/hooks";
+import { patch } from "@web/core/utils/patch";
+import { StatusBarField } from "@web/views/fields/statusbar/statusbar_field";
+const PhoneCallDetails = require('voip.PhoneCallDetails');
+import { WarningDialog } from "@web/core/errors/error_dialogs";
+
+patch(PhoneCallDetails.prototype, "ata_voip.PhoneCallDetails", {
+     async _onClickToPartner(ev) {
+        const _super = this._super.bind(this);
+        await Promise.resolve();
+
+        ev.preventDefault();
+        if (!config.device.isMobileDevice) {
+            this.trigger_up('fold_panel');
+        }
+        let resId = this.partnerId;
+        if (!this.partnerId) {
+            let domain = [];
+            if (this.phoneNumber && this.mobileNumber) {
+                domain = ['|',
+                    ['phone', '=', this.phoneNumber],
+                    ['mobile', '=', this.mobileNumber]];
+            } else if (this.phoneNumber) {
+                domain = ['|',
+                    ['phone', '=', this.phoneNumber],
+                    ['mobile', '=', this.phoneNumber]];
+            } else if (this.mobileNumber) {
+                domain = [['mobile', '=', this.mobileNumber]];
+            }
+            const ids = await this._rpc({
+                method: 'search_read',
+                model: "res.partner",
+                kwargs: {
+                    domain,
+                    fields: ['id'],
+                    limit: 1,
+                }
+            });
+            if (ids.length) {
+                resId = ids[0].id;
+            }
+        }
+        if (resId !== undefined) {
+            this.do_action({
+                res_id: resId,
+                res_model: "res.partner",
+                target: 'new',
+                type: 'ir.actions.act_window',
+                views: [[false, 'form']],
+            }, {
+                fullscreen: config.device.isMobile
+            });
+        } else {
+            const context = {};
+            if (this.phoneNumber) {
+                context.phoneNumber = this.phoneNumber;
+            }
+            if (this.email) {
+                context.email = this.email;
+            }
+            if (this.mobileNumber) {
+                context.mobileNumber = this.mobileNumber;
+            }
+            context['default_phone'] = this.phoneNumber //+ATA Boretskiy
+            this.do_action({
+                context,
+                res_model: 'res.partner',
+                target: 'new',
+                type: 'ir.actions.act_window',
+                views: [[false, 'form']],
+            }, {
+                fullscreen: config.device.isMobile
+            });
+        }
+    },
+});
