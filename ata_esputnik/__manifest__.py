488d4426753bb08fa3422cf3f634288c57089891
Av1d <av1d1ty@pm.me>
2023-11-21 16:16:25 +0200

    [IMP] ata_esputnik: add configuration page

--- ata_esputnik/__manifest__.py
+++ ata_esputnik/__manifest__.py
@@ -3,7 +3,7 @@
     'summary': '''
         Send data using eSputnik API
     ''',
-    'version': '16.0.1.0.0',
+    'version': '16.0.1.0.1',
     'author': 'ToDo',
     'website': 'https://it-artel.ua',
     'license': 'OPL-1',
@@ -11,6 +11,7 @@
     'depends': [
     ],
     'data': [
+        'views/res_config_settings_views.xml',
     ],
     "external_dependencies": {
         "python": ['requests', 'json'],
--- ata_esputnik/models/__init__.py
+++ ata_esputnik/models/__init__.py
@@ -1,1 +1,2 @@
 from . import esputnik_request
+from . import res_config_settings
