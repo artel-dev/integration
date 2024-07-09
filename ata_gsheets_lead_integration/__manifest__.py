a0a4a5af48062bb132d53413fd39a6111d5a59d4
Igor Kutko <gkm0123@gmail.com>
2024-07-09 12:17:21 +0300

    [FIX] ata_gsheets_lead_integration: fixed singleton error

--- ata_gsheets_lead_integration/__manifest__.py
+++ ata_gsheets_lead_integration/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Google sheets lead integration',
     'summary': 'Google sheets lead integration',
-    'version': '16.0.1.0.0',
+    'version': '16.0.1.0.1',
     'author': 'ToDo',
     'website': 'https://it-artel.ua/',
     'license': 'OPL-1',
--- ata_gsheets_lead_integration/models/mail_activity.py
+++ ata_gsheets_lead_integration/models/mail_activity.py
@@ -64,36 +64,66 @@ class MailActivity(models.Model):
                      ]]}]}).execute()
 
     def _action_done(self, feedback=False, attachment_ids=None):
-        import_dict = {
-            'activity_type_id': self.activity_type_id.name if self.activity_type_id else '',
-            'date': datetime.datetime.strftime(fields.datetime.now(),
-                                               '%d.%m.%Y') if fields.datetime.now() else '',
-        }
-        res_model = self.res_model
-
-        res = super()._action_done(feedback=feedback, attachment_ids=attachment_ids)
-
-        if res_model != 'crm.lead':
-            return res
-
         with_user = self.env['ir.config_parameter'].sudo()
         ata_active = with_user.get_param('ata_gsheets_lead_integration.ata_lead_active')
 
-        if isinstance(res, tuple):
-            if res[0] and res[0]._name == 'mail.message':
-                import_dict['id'] = res[0].id
-                import_dict['user_id'] = res[0].author_id.name if res[0].author_id else ''
+        dt = datetime.datetime
+        done_date = fields.datetime.now()
+        done_date_fmt = dt.strftime(done_date, '%d.%m.%Y') if done_date else ''
+
+        activities = list()
+        res_models = list()
+        for record in self:
+            import_dict = dict()
+            import_dict['activity_type_id'] = (
+                record.activity_type_id.name if record.activity_type_id else ''
+            )
+            import_dict['date'] = done_date_fmt
+            activities.append(import_dict)
+            res_models.append(record.res_model)
+
+        # res = super()._action_done(feedback=feedback, attachment_ids=attachment_ids)
+        messages, next_activities = super()._action_done(
+            feedback=feedback, attachment_ids=attachment_ids)
+
+        if 'crm.lead' not in res_models or not ata_active:
+            return messages, next_activities
+
+        # if isinstance(res, tuple):
+        #     if res[0] and res[0]._name == 'mail.message':
+        #         import_dict['id'] = res[0].id
+        #         import_dict['user_id'] = res[0].author_id.name if res[0].author_id else ''
+        # else:
+        #     _logger.error(f"res don't contain 'mail.message'")
+        #     return res
+
+        if messages and messages._name == 'mail.message':
+            for import_dict, res_model, message in zip(
+                    activities, res_models, messages):
+                if res_model != 'crm.lead':
+                    continue
+                import_dict['id'] = message.id
+                import_dict['user_id'] = (
+                    message.author_id.name if message.author_id else ''
+                )
+                try:
+                    self.write_lead_to_google_sheet(
+                        import_dict,
+                        action='create'
+                    )
+                except Exception as exc:
+                    _logger.error(f"unlink error: {exc}")
         else:
             _logger.error(f"res don't contain 'mail.message'")
-            return res
+            return messages, next_activities
 
-        if ata_active:
-            try:
-                self.write_lead_to_google_sheet(
-                    import_dict,
-                    action='create'
-                )
-            except Exception as exc:
-                _logger.error(f"unlink error: {exc}")
+        # if ata_active:
+        #     try:
+        #         self.write_lead_to_google_sheet(
+        #             import_dict,
+        #             action='create'
+        #         )
+        #     except Exception as exc:
+        #         _logger.error(f"unlink error: {exc}")
 
-        return res
+        return messages, next_activities
