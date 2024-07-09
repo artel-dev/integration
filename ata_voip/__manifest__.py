2aed1693bfb71a1cbfd088cf753a1ace84598b83
Igor Kutko <gkm0123@gmail.com>
2024-07-10 00:01:28 +0300

    [UPD] ata_voip: converted to version 17.0

--- ata_voip/__manifest__.py
+++ ata_voip/__manifest__.py
@@ -1,20 +1,20 @@
 {
     'name': 'VOIP customization',
     'summary': 'VOIP customization',
-    'version': '16.0.1.0.2',
+    'version': '17.0.1.0.0',
     'category': 'VOIP',
     'author': 'ToDo',
-    'website': 'https://it-artel.ua',
+    'website': 'https://todo.ltd',
     'license': 'OPL-1',
-    'depends': ['base', 'voip', 'mail', 'web', 'web_mobile'],
+    'depends': [
+        'voip',
+        'web'
+    ],
     'installable': True,
     'application': True,
     'assets': {
-        'mail.assets_messaging': [
-            'ata_voip/static/src/models/*.js',
-        ],
         'web.assets_backend': [
-            'ata_voip/static/src/js/ata_phone_call_details.js',
+            "ata_voip/static/src/**/*",
         ],
     },
 }
