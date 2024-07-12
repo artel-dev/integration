4814c5a359ed0b67d1651611ecd6a36165054f97
Igor Kutko <gkm0123@gmail.com>
2024-07-13 01:48:12 +0300

    [FIX] ata_voip: fixed searching partner by phone

--- ata_voip/models/voip_call.py
+++ ata_voip/models/voip_call.py
@@ -71,3 +71,40 @@ class AtaVoipCall(models.Model):
                         found_partner.message_post(body=body)
 
         return super().write(vals)
+
+    def get_contact_info(self):
+        self.ensure_one()
+        number = self.phone_number
+        # Internal extensions could theoretically be one or two digits long.
+        # phone_mobile_search doesn't handle numbers that short: do a regular
+        # search for the exact match:
+        if len(number) < 3:
+            domain = ["|", ("phone", "=", number), ("mobile", "=", number)]
+        # 00 and + both denote an international prefix. phone_mobile_search will
+        # match both indifferently.
+        elif number.startswith(("+", "00")):
+            domain = [("phone_mobile_search", "=", number)]
+        # USA: Calls between different area codes are usually prefixed with 1.
+        # Conveniently, the country code for the USA also happens to be 1, so we
+        # just need to add the + symbol to format it like an international call
+        # and match what's supposed to be stored in the database.
+        elif number.startswith("1"):
+            domain = [("phone_mobile_search", "=", f"+{number}")]
+        # 0 is the national prefix recommended by the ITU-T. This means that a
+        # number starting with 0 will identify a national call in most
+        # countries. We don't know the country code, so we can't convert the
+        # number to the international format: Replace the prefix with a
+        # wildcard.
+        elif number.startswith("0"):
+            domain = [("phone_mobile_search", "=like", f"%{number[1:]}")]
+        # The phone number doesn't match any expected format. Could it be some
+        # kind of internal extension? Try looking for an exact match:
+        else:
+            # domain = [("phone_mobile_search", "=", number)]
+            number = '+' + number
+            domain = [("phone_mobile_search", "=", number)]
+        partner = self.env["res.partner"].search(domain, limit=1)
+        if not partner:
+            return False
+        self.partner_id = partner
+        return self.partner_id._format_contacts()[0]
