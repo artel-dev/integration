ed75f2eebb930267c4231180b290d460efed6c25
gnezamay <gnezamay@it-artel.ua>
2025-01-27 09:58:47 +0200

    [MOD] ata_external_connection add category

--- ata_external_connection/__manifest__.py
+++ ata_external_connection/__manifest__.py
@@ -1,23 +1,24 @@
 {
     'name': 'External connection',
     'summary': 'External connection',
-    'version': '16.0.1.0.9',
+    'version': '16.0.2.0.9',
     'author': 'ToDo',
     'website': 'it-artel.ua',
     'license': 'OPL-1',
-    'category': 'Integration',
+    'category': 'Integration/Base',
     'depends': [
         'base',
     ],
     'data': [
+        'data/ata_exchange_category.xml',
+        'data/ata_exchange_queue_cron.xml',
         'security/ir.model.access.csv',
         'views/ata_external_connection_menus.xml',
         'views/ata_external_connection_domain.xml',
         'views/ata_external_system_views.xml',
         'views/ata_exchange_log_views.xml',
         'views/ata_exchange_queue_views.xml',
-        'views/ata_exchange_queue_usage_views.xml',
-        'data/ata_exchange_queue_cron.xml',
+        'views/ata_exchange_queue_usage_views.xml',      
     ],
     'installable': 'True',
 }
