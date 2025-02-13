62de5eed8cc2bb61f860d96231904409d6d3d66c
gnezamay <gnezamay@it-artel.ua>
2025-02-13 08:50:03 +0200

    [FIX] ata_exchange_v3 add method in filter of queue

--- ata_exchange_v3/__manifest__.py
+++ ata_exchange_v3/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Exchange between systems',
     'summary': 'Exchange between systems (ATA)',
-    'version': '16.0.3.0.4',
+    'version': '16.0.3.0.5',
     'author': 'gnezamay',
     'website': 'todo.ltd',
     'license': 'OPL-1',
