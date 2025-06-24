56f4bfe1a92af97a7a7edec1aaf3f0f5aaa50dcc
gnezamay <gnezamay@todo.ltd>
2025-06-24 07:51:19 +0000

    [MOD] ata_exchange extend description

--- ata_exchange_ftp/__manifest__.py
+++ ata_exchange_ftp/__manifest__.py
@@ -1,15 +1,24 @@
 {
     'name': 'Exchange between systems. FTP Extension',
-    'description': 'Exchange between systems. FTP Extension',
+    'summary': 'Adds FTP support for the Data Exchange module.',
+    'description': """
+This module extends the 'Data Exchange' functionality to allow integration with FTP servers.
+Key features:
+- Configure FTP connections (server, port, credentials).
+- Test the connection to the FTP server directly from Odoo.
+- List and download files from specified directories.
+- Automatically create folders and move files on the FTP server.
+- Seamlessly integrates into the existing exchange system workflows.
+    """,
     'version': '17.0.1.4.0',
     'author': 'ToDo',
     'website': 'https://todo.ltd',
     'license': 'OPL-1',
     'category': 'Integration/Extension',
     'depends': [
-        'ata_exchange_v4',        
+        'ata_exchange_v4',
     ],
-    'data': [],    
+    'data': [],
     'external_dependencies': {},
-    'installable': 'True',
+    'installable': True,
 }
--- ata_exchange_xlsx/__manifest__.py
+++ ata_exchange_xlsx/__manifest__.py
@@ -1,17 +1,28 @@
 {
     'name': 'Exchange between systems. XLSX Parser Extension',
-    'description': 'Exchange between systems. XLSX Parser Extension',
+    'summary': 'Provides a utility for parsing XLSX files.',
+    'description': """
+This module provides an abstract model to handle XLSX file parsing.
+It uses the pandas library to read and process Excel files, offering a flexible way to extract data.
+
+Key Features:
+- Parses XLSX files into a pandas DataFrame.
+- Allows specifying rows to skip at the beginning of the file.
+- Maps Excel columns to specific field names.
+- Validates the presence of required columns.
+- Designed to be inherited by other models that need to process XLSX data as part of an exchange workflow.
+    """,
     'version': '17.0.1.4.0',
     'author': 'ToDo',
     'website': 'https://todo.ltd',
     'license': 'OPL-1',
     'category': 'Integration/Extension',
     'depends': [
-        'ata_exchange_v4',        
+        'ata_exchange_v4',
     ],
-    'data': [],    
+    'data': [],
     'external_dependencies': {
         'python': ['openpyxl', 'pandas'],
     },
-    'installable': 'True',
+    'installable': True,
 }
